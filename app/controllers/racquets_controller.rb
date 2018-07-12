54ef5415aab67458e4671b448517832da558460c
suzy baldwin <gbaldwin24@comcast.net>
2018-07-12 15:30:32 +0000

    racquet edit

--- app/controllers/racquets_controller.rb
+++ app/controllers/racquets_controller.rb
@@ -12,4 +12,11 @@ class RacquetsController < ApplicationController
     erb :'racquets/new'
   end
 
+  get "/racquets/:id/edit" do
+    redirect_if_not_logged_in
+    @error_message = params[:error]
+    @racquet = Racquet.find(params[:id])
+    erb :'racquets/edit'
+  end
+
 end
