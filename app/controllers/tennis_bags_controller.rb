7ee7e26cb77514c283220afb735022b810b29dfb
suzy baldwin <gbaldwin24@comcast.net>
2018-07-12 15:09:07 +0000

    tennis bag controller and view

--- app/controllers/tennis_bags_controller.rb
+++ app/controllers/tennis_bags_controller.rb
@@ -6,6 +6,19 @@ class TennisBagsController < ApplicationController
     erb :'tennis_bags/index'
   end
 
+  get "/tennis_bags/new" do
+     redirect_if_not_logged_in
+     @error_message = params[:error]
+     erb :'tennis_bags/new'
+   end
+
+   get "/tennis_bags/:id/edit" do
+    redirect_if_not_logged_in
+    @error_message = params[:error]
+    @tennis_bag = TennisBag.find(params[:id])
+    erb :'tennis_bags/edit'
+  end
+
 
 
 
