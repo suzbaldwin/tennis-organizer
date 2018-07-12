8433a7aa68853eda1476f4b0213e117fe78c3ea4
suzy baldwin <gbaldwin24@comcast.net>
2018-07-12 15:02:48 +0000

    tennis bag controller and view

--- app/controllers/tennis_bags_controller.rb
+++ app/controllers/tennis_bags_controller.rb
@@ -1,5 +1,11 @@
 class TennisBagsController < ApplicationController
 
+  get "/tennis_bags" do
+    redirect_if_not_logged_in
+    @tennis_bags = TennisBag.all
+    erb :'tennis_bags/index'
+  end
+
 
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,4 +37,14 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/logout' do
+    if session[:user_id] != nil
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
+
 end
