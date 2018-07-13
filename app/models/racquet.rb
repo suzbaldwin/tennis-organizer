b1fde45435afca52510cdaff8084d388c0cc463a
suzy baldwin <gbaldwin24@comcast.net>
2018-07-13 17:37:01 +0000

    models and views

--- app/models/racquet.rb
+++ app/models/racquet.rb
@@ -1,4 +1,8 @@
 class Racquet < ActiveRecord::Base
   belongs_to :tennis_bag
 
+  def self.valid_params?(params)
+    return !params[:name].empty? && !params[:manufacturer].empty?
+  end
+
 end
--- app/models/tennis_bag.rb
+++ app/models/tennis_bag.rb
@@ -2,4 +2,8 @@ class TennisBag < ActiveRecord::Base
   has_many :racquets
   belongs_to :user
 
+  def self.valid_params?(params)
+    return !params[:name].empty? && !params[:capacity].empty?
+  end
+
 end
